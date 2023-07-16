274d720cd8fae3c4ecbf699f347192a09d78846d
sweatpantsdanny <d.hamilton@ufl.edu>
2023-07-15 23:24:05 -0400

    Explosion particle system

--- LaserDefenderProj/Assets/Scripts/Health.cs
+++ LaserDefenderProj/Assets/Scripts/Health.cs
@@ -7,6 +7,7 @@ public class Health : MonoBehaviour
     private const int DEFAULT_HEALTH = 5;
 
     [SerializeField] private int health = DEFAULT_HEALTH;
+    [SerializeField] private ParticleSystem hitEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,11 +19,28 @@ public class Health : MonoBehaviour
         }
 
         // Take damage and tell damage dealer that it hit something.
+        PlayHitEffect();
+        TakeDamage(damageDealer);
+        damageDealer.Hit();
+    }
+
+    private void TakeDamage(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();
         if (health <= 0)
         {
             Destroy(gameObject);
         }
-        damageDealer.Hit();
+    }
+
+    void PlayHitEffect()
+    {
+        if (hitEffect == null)
+        {
+            return;
+        }
+
+        ParticleSystem explosionEffect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(explosionEffect, explosionEffect.main.duration + explosionEffect.main.startLifetime.constantMax);
     }
 }
