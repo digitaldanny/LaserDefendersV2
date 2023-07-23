7c6959885189204c6fb5e4fff7257c80f22d8908
sweatpantsdanny <d.hamilton@ufl.edu>
2023-07-22 21:36:02 -0400

    Implemented basic enemy AI + added public APIs to the shooter script

--- LaserDefenderProj/Assets/Scripts/Character/Player.cs
+++ LaserDefenderProj/Assets/Scripts/Character/Player.cs
@@ -64,7 +64,7 @@ public class Player : MonoBehaviour
             return;
         }
 
-        shooter.SetEnableShooting(value.isPressed);
+        shooter.FireContinuously(value.isPressed);
     }
 
     /*
